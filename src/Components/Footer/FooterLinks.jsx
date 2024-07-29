8163f66bd92806f96172f430eef9a5fab5e9d5c8
rencyngina <rencyngina@gmail.com>
2024-07-29 03:57:14 +0300

    paypal

---
+++ src/Components/Footer/FooterLinks.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+// const FooterLinks = () => {
+//   return (
+//     <div className='bg-[#617ABC] lg:mt- mt-10 lg:mb- mb-0'>
+//       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+//         <div className='lg:h-20 h-20'>
+//           {/* Add your footer content here */}
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default FooterLinks;
